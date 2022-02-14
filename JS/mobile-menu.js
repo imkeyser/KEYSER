8c2aa305dac3a4fa79dad0ff27cee3e71fa95df2
Abeille <tabeille@albmolecular.com>
2022-02-14 11:23:29 +0100

    Ajout fonctions menu mobile et formulaire de contact

--- JS/mobile-menu.js
+++ JS/mobile-menu.js
@@ -1,18 +1,28 @@
 $('document').ready(function(){
-
     $isMobileMenuActive = true;
-
     $('#mobile-menu').click(function(){
 
         if($isMobileMenuActive){
             $isMobileMenuActive = false;
-            $('main').hide(80);
+            $('#main-container').hide(80);
+            $('#nav__mobile').show(80);
+            $('#nav__mobile').css('display', 'flex');
+            
         }
         else {
             $isMobileMenuActive = true;
-            $('main').show(80);
+            $('#nav__mobile').hide(80);
+            $('#main-container').show(80);
         }
         
         return false;
     });
+
+    $('#mobile_nav_link a').click(function(){
+        $isMobileMenuActive = true;
+        $('#nav__mobile').hide(80);
+        $('#main-container').show(80);
+    });
+
+
 });
